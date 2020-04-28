a60752be16a1e2f98825055a5c5c83117940d8a1
ashamekhi@careandcarers.com <ali_shmki@yahoo.com>
2020-04-28 11:03:07 +0100

    o

---
+++ ConsoleTemp/Program.cs
@@ -0,0 +1,29 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleTemp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            client.SetBearerToken("access_token");
+            var response = await client.GetAsync("url");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+
+
+        } 
+    }
+}
